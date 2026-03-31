81bb81eb509dffb6012abc8e1e774ec7f1f1dedc
benginvit <benjamin@invit.se>
2026-03-31 07:25:16 +0000

    wip

---
+++ BengiDevTools/Platforms/Windows/App.cs
@@ -0,0 +1,23 @@
+using Microsoft.UI.Dispatching;
+using Microsoft.UI.Xaml;
+using WinRT;
+
+namespace BengiDevTools.WinUI;
+
+public class App : MauiWinUIApplication
+{
+    [global::System.STAThreadAttribute]
+    static void Main(string[] args)
+    {
+        ComWrappersSupport.InitializeComWrappers();
+        Application.Start(p =>
+        {
+            var context = new DispatcherQueueSynchronizationContext(
+                DispatcherQueue.GetForCurrentThread());
+            SynchronizationContext.SetSynchronizationContext(context);
+            _ = new App();
+        });
+    }
+
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
